38b61671c5620722a11a1307b9a9612399c42493
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-17 12:51:16 +0200

    Add files via upload

---
+++ ComputerArchitecture/Computer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        private List<CPU> multiprocessor;
+
+        public Computer(string model, int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+            Multiprocessor = new List<CPU>();
+        }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public List<CPU> Multiprocessor { get; set; }
+
+        public int Count => Multiprocessor.Count;//!!!!!!!!!!!!!!
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count < Capacity)
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            if (Multiprocessor.Any(c => c.Brand == brand))
+            {
+                Multiprocessor.RemoveAll(c => c.Brand == brand);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public CPU MostPowerful()
+        {
+
+            return Multiprocessor.OrderByDescending(c => c.Frequency).FirstOrDefault();
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            return Multiprocessor.Find(c => c.Brand == brand);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"CPUs in the Computer {Model}:");
+            foreach (CPU cpu in Multiprocessor)
+            {
+                sb.AppendLine(cpu.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
